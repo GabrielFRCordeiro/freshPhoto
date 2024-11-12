0f17ca393684c18124572cb0030efb45e71dfa16
Gabriel Cordeiro <120519526+GabrielFRCordeiro@users.noreply.github.com>
2024-11-12 09:17:03 -0300

    feat(Navegacao): tornando navegacao responsiva

--- view/src/Components/Navegacao/index.jsx
+++ view/src/Components/Navegacao/index.jsx
@@ -3,11 +3,11 @@ import './Navegacao.css';
 export default function Navegacao() {
   return (
     <nav className='navbar navegacao fs-1'>
-        <div className="container">
-            <i className="bi bi-house-door navIcon"></i>
-            <i className="bi bi-search navIcon"></i>
-            <i className="bi bi-hand-thumbs-up navIcon"></i>
-            <i className="bi bi-person-circle navIcon navIconFocus"></i>
+        <div className="container d-lg-flex flex-lg-column justify-content-lg-aroundcontainer d-lg-flex flex-lg-column justify-content-lg-start">
+            <i className="bi bi-house-door navIcon mt-lg-5"></i>
+            <i className="bi bi-search navIcon mt-lg-4"></i>
+            <i className="bi bi-hand-thumbs-up navIcon mt-lg-4"></i>
+            <i className="bi bi-person-circle navIcon navIconFocus mt-lg-4"></i>
         </div>
     </nav>
   )
