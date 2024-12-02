56cbfab2d4345c14c0990c547dddbb69adb0b80f
Gabriel Cordeiro <120519526+GabrielFRCordeiro@users.noreply.github.com>
2024-12-01 23:18:53 -0300

    feat(perfil): estilizando user_img

--- frontend/js/user_perfil.js
+++ frontend/js/user_perfil.js
@@ -23,13 +23,19 @@ window.addEventListener('load', async (e) => {
             const user_img_padrao = localStorage.getItem(`${user.usuario} img`)
             if (user_img_padrao) {
                 const user_img_especifica = document.createElement('svg');
-                user_img_especifica.innerHTML = `<svg class="user_img pe-4 pe-lg-3" viewBox="0 0 45 44" fill="none" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-    <rect x="0.844727" y="0.0361328" width="43.4778" height="43.4778" fill="url(#pattern0_484_471)"/>
-    <defs>
+                user_img_especifica.innerHTML = `<svg class="user_img pe-4 pe-lg-3" viewBox="0 0 360 360" fill="none" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+    
+    
+    
+    <image id="image0_484_471" width="100%" height="100%" xlink:href="${localStorage.getItem(user.usuario + ' img')}" clip-path="url(#clip-circle)" preserveAspectRatio="xMidYMid slice"></image>
+    <defs><clipPath id="clip-circle">
+            <circle cx="180" cy="180" r="170"></circle>
+        </clipPath>
+    
     <pattern id="pattern0_484_471" patternContentUnits="objectBoundingBox" width="1" height="1">
-    <use xlink:href="#image0_484_471" transform="scale(0.0208333)"/>
+    <use xlink:href="#image0_484_471" transform="scale(0.0208333)"></use>
     </pattern>
-    <image id="image0_484_471" width="48" height="48" xlink:href="${localStorage.getItem(`${user.usuario} img`)}"/>
+    
     </defs>
     </svg>`
                 user_img.parentNode.replaceChild(user_img_especifica, user_img)
