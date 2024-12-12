bad8e0c0dd0f1f8187e5350611e521b0818ebcd1
Gabriel Cordeiro <120519526+GabrielFRCordeiro@users.noreply.github.com>
2024-12-12 11:19:06 -0300

    feat(perfil): exibindo cards do usuario

--- frontend/js/exibir_cards.js
+++ frontend/js/exibir_cards.js
@@ -1,21 +1,24 @@
 const feed_cards = document.querySelector('#feed_cards');
 
-const API_URL = 'http://127.0.0.1:5000/postagem'
+const API_URL = 'http://127.0.0.1:5000/usuario/card_perfil';
 
 window.addEventListener('load', async (e) => {
     e.preventDefault();
 
-    const response = await fetch(API_URL);
+    usuario = sessionStorage.getItem('usuario');
+    const response = await fetch(API_URL, {
+        body: usuario
+    });
     const postagens = await response.json();
 
-    let cards_teste = []
-    postagens.forEach(postagem => {
-        if (postagem.usuario == sessionStorage.getItem("usuario")) {
-            cards_teste.push(postagem.foto);
-        }
-    });
-    console.log(cards_teste)
-    show_cards(cards_teste);
+    // let cards_teste = []
+    // postagens.forEach(postagem => {
+    //     if (postagem.usuario == sessionStorage.getItem("usuario")) {
+    //         cards_teste.push(postagem.foto);
+    //     }
+    // });
+    // console.log(cards_teste)
+    show_cards(postagens);
 })
 
 function show_cards(cards) {
