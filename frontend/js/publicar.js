29809ca58875d244585f9d48134deaa87f834ccf
Gabriel Cordeiro <120519526+GabrielFRCordeiro@users.noreply.github.com>
2024-12-12 11:43:22 -0300

    feat(publicar): vinculando front com back

--- frontend/js/publicar.js
+++ frontend/js/publicar.js
@@ -5,76 +5,69 @@ const t_field_receita = document.querySelector('#t_field_receita');
 const btn_publicar = document.querySelector('#btn_publicar');
 const form_publicar = document.querySelector("#form_publicar");
 
-const API_URL_RECEITA = 'http://127.0.0.1:5000/receita';
-const API_URL_USUARIO = 'http://127.0.0.1:5000/usuario';
+// const API_URL_RECEITA = 'http://127.0.0.1:5000/receita';
+// const API_URL_USUARIO = 'http://127.0.0.1:5000/usuario';
 const API_URL_POSTAGEM = 'http://127.0.0.1:5000/postagem';
-const API_URL_CATEGORIA = 'http://127.0.0.1:5000/categoria';
+// const API_URL_CATEGORIA = 'http://127.0.0.1:5000/categoria';
 
 form_publicar.addEventListener('submit', async (e) => {
     e.preventDefault();
 
-    const receita = {texto: t_field_receita.value}
+    // const receita = {texto: t_field_receita.value}
 
-    await fetch(API_URL_RECEITA, {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(receita)
-    });
+    // await fetch(API_URL_RECEITA, {
+    //     method: 'POST',
+    //     headers: { 'Content-Type': 'application/json' },
+    //     body: JSON.stringify(receita)
+    // });
 
-    const response_users = await fetch(API_URL_USUARIO);
-    const users = await response_users.json();
+    // const response_users = await fetch(API_URL_USUARIO);
+    // const users = await response_users.json();
 
-    let id_usuario = '';
+    // let id_usuario = '';
 
-    users.forEach(user => {
-        if (user.usuario === sessionStorage.getItem('usuario')) {
-            id_usuario = user.id;
-        }
-    });
+    // users.forEach(user => {
+    //     if (user.usuario === sessionStorage.getItem('usuario')) {
+    //         id_usuario = user.id;
+    //     }
+    // });
 
-    const response_categorias = await fetch(API_URL_CATEGORIA);
-    const categorias = await response_categorias.json();
+    // const response_categorias = await fetch(API_URL_CATEGORIA);
+    // const categorias = await response_categorias.json();
 
-    let id_categoria = '';
+    // let id_categoria = '';
 
-    categorias.forEach(categoria => {
-        if (categoria.nome === input_categoria.value) {
-            id_categoria = categoria.id;
-        }
-    });
+    // categorias.forEach(categoria => {
+    //     if (categoria.nome === input_categoria.value) {
+    //         id_categoria = categoria.id;
+    //     }
+    // });
 
-    const response_receitas = await fetch(API_URL_RECEITA);
-    const receitas = await response_receitas.json();
+    // const response_receitas = await fetch(API_URL_RECEITA);
+    // const receitas = await response_receitas.json();
 
-    let id_receita = '';
+    // let id_receita = '';
 
-    receitas.forEach(receita => {
-        if (receita.texto === t_field_receita.value) {
-            id_receita = receita.id;
-        }
-    });
-
-    const nova_publicacao = {
-        imagem: input_img.value,
-        categoria: input_categoria.value,
-        legenda: t_field_legenda.value,
-        receita: t_field_receita.value
-    };
+    // receitas.forEach(receita => {
+    //     if (receita.texto === t_field_receita.value) {
+    //         id_receita = receita.id;
+    //     }
+    // });
 
     const formData = new FormData();
-    formData.append('id_usuario', id_usuario);
-    formData.append('id_categoria', id_categoria);
-    formData.append('foto', input_img.files[0]);
+    formData.append('usuario', sessionStorage.getItem('usuario'));
+    formData.append('categoria', input_categoria.value);
+    formData.append('img', input_img.files[0]);
     formData.append('legenda', t_field_legenda.value);
-    formData.append('id_receita', id_receita);
+    formData.append('receita', t_field_receita.value);
 
     if (!input_img.files.length) {
         console.error('Por favor, selecione uma imagem.');
         return; // Impede o envio se não houver arquivo
     }
 
-    if (!id_usuario || !id_categoria || !id_receita) {
-        console.error('ID de usuário, categoria ou receita não encontrados.');
+    if (!input_categoria.value) {
+        console.error('Por favor, selecione uma categoria.');
         return; // Impede o envio se algum ID estiver vazio
     }
 
