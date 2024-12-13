f0a11e23e240360f19785451da4b86308f0bb9e2
Gabriel Cordeiro <120519526+GabrielFRCordeiro@users.noreply.github.com>
2024-12-13 18:30:36 -0300

    feat+fix(publicar): fazendo modal

--- frontend/js/modal.js
+++ frontend/js/modal.js
@@ -1,12 +1,17 @@
 var modal_postar = document.querySelector("#modal_postar");
 var btn_postar = document.querySelector("#btn_postar");
+var btn_modal = document.querySelector("#btn_modal");
 
 btn_postar.onclick = () => {
-  modal_postar.style.display = "block";
+  modal_postar.style.display = "flex";
+}
+
+btn_modal.onclick = () => {
+  modal_postar.style.display = "none";
 }
 
 window.onclick = e => {
-  if (e.target == modal_postar) {
+  if ((e.target == modal_postar) || (e.target == form_publicar)) {
     modal_postar.style.display = "none";
   }
 }
