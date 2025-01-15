99fd0cb7ce51d535158e97c080760616bc9eabe6
Gabriel Cordeiro <120519526+GabrielFRCordeiro@users.noreply.github.com>
2025-01-15 11:49:43 -0300

    fix(modal): corrigindo bug de fechar modais

--- frontend/js/modal.js
+++ frontend/js/modal.js
@@ -1,6 +1,8 @@
 const modal_postar = document.querySelector("#modal_postar");
+const modal_form = document.querySelector("#form_publicar");
 const btn_postar = document.querySelector("#btn_postar");
 const btn_modal = document.querySelector("#btn_modal");
+const btn_modal_receita = document.querySelector("#btn_modal_receita");
 const modal_config = document.querySelector("#modal_config");
 const btn_menu_desktop = document.querySelector("#btn_menu_desktop");
 const btn_menu_mobile = document.querySelector("#btn_menu_mobile");
@@ -17,12 +19,6 @@ if (modal_postar) {
   btn_modal.onclick = () => {
     modal_postar.style.display = "none";
   }
-  
-  window.onclick = e => {
-    if ((e.target == modal_postar) || (e.target == form_publicar)) {
-      modal_postar.style.display = "none";
-    }
-  }
 }
 
 if (modal_config && sessao) {  
@@ -35,14 +31,6 @@ if (modal_config && sessao) {
     btn_menu_desktop.classList.replace('d-none', 'd-inline-block');
     btn_menu_mobile.classList.replace('d-inline-block', 'd-none');
   }
-  
-  window.onclick = e => {
-    if (e.target == modal_config) {
-      modal_config.style.display = "none";
-      btn_menu_desktop.classList.replace('d-inline-block', 'd-none');
-      btn_menu_mobile.classList.replace('d-none', 'd-inline-block');
-    }
-  } 
 } else {
   btn_menu_mobile.onclick = () => {
     modal_entrar.classList.replace('d-none', 'd-inline-block');
@@ -52,14 +40,6 @@ if (modal_config && sessao) {
     btn_menu_desktop.classList.replace('d-none', 'd-inline-block');
     btn_menu_mobile.classList.replace('d-inline-block', 'd-none');
   }
-
-  window.onclick = e => {
-    if (e.target == modal_entrar) {
-      modal_entrar.classList.replace('d-inline-block', 'd-none');
-      btn_menu_desktop.classList.replace('d-inline-block', 'd-none');
-      btn_menu_mobile.classList.replace('d-none', 'd-inline-block');
-    }
-  } 
 }
 
 if (modal_receita) {
@@ -69,9 +49,34 @@ if (modal_receita) {
     }
   });
 
-  window.onclick = e => {
-    if (e.target == modal_receita) {
+  btn_modal_receita.onclick = () => {
+    modal_receita.style.display = "none";
+  }
+}
+
+window.onclick = e => {
+  switch (e.target) {
+    case modal_receita:
       modal_receita.style.display = "none";
-    }
+      break;
+
+    case modal_entrar:
+      modal_entrar.classList.replace('d-inline-block', 'd-none');
+      btn_menu_desktop.classList.replace('d-inline-block', 'd-none');
+      btn_menu_mobile.classList.replace('d-none', 'd-inline-block');
+      break;
+
+    case modal_postar:
+      modal_postar.style.display = "none";
+      break;
+
+    case modal_config:
+      modal_config.style.display = "none";
+      btn_menu_desktop.classList.replace('d-inline-block', 'd-none');
+      btn_menu_mobile.classList.replace('d-none', 'd-inline-block');
+      break;
+  
+    default:
+      break;
   }
 }
