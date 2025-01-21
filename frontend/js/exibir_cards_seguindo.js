08621bea6a601d04e26b0d62e2c66061d378551a
Gabriel Cordeiro <120519526+GabrielFRCordeiro@users.noreply.github.com>
2025-01-21 11:45:17 -0300

    feat(exibir_cards): criando funcao para cada pag

---
+++ frontend/js/exibir_cards_seguindo.js
@@ -0,0 +1,33 @@
+const feed_cards = document.querySelector('#feed_cards');
+
+const API_URL = 'http://127.0.0.1:5000/usuario/card_seguidores';
+
+window.addEventListener('load', async (e) => {
+    e.preventDefault();
+
+    usuario = sessionStorage.getItem('usuario');
+    const response = await fetch(API_URL, {
+        body: usuario
+    });
+    const postagens = await response.json();
+
+    // let cards_teste = []
+    // postagens.forEach(postagem => {
+    //     if (postagem.usuario == sessionStorage.getItem("usuario")) {
+    //         cards_teste.push(postagem.foto);
+    //     }
+    // });
+    // console.log(cards_teste)
+    show_cards(postagens);
+})
+
+function show_cards(cards) {
+    feed_cards.innerHTML = ''
+    cards.forEach(card => {
+        feed_cards.innerHTML += `<a class="feed_card" href="./user-feed.html">
+    <div class="feed_card">
+        <img src="${card}" alt="">
+    </div>
+</a>`
+    });
+}
