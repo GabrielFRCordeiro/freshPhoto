fe122e0630624e1fb231d8bb9766d3b58452fd57
Gabriel Cordeiro <120519526+GabrielFRCordeiro@users.noreply.github.com>
2025-01-21 16:25:18 -0300

    feat(modal receita): criando componente de receita

--- frontend/src/routes/Seguindo.jsx
+++ frontend/src/routes/Seguindo.jsx
@@ -2,6 +2,7 @@ import Card from "../components/Card"
 import img_teste_1 from "../assets/imgs/img_teste_1.png"
 import img_teste_2 from "../assets/imgs/img_teste_2.png"
 import img_teste_3 from "../assets/imgs/img_teste_3.png"
+import Receita from "../components/Receita"
 
 let fotos = [img_teste_3, img_teste_1, img_teste_2, img_teste_3, img_teste_1, img_teste_2, img_teste_3, img_teste_1, img_teste_2]
 
@@ -13,6 +14,9 @@ function Seguindo() {
         {fotos.map(foto => (
           <Card post_img={foto} />
         ))}
+      </section>
+      <section id="modal_receita" class="modal align-items-center">
+        <Receita />
       </section>
     </>
   )
