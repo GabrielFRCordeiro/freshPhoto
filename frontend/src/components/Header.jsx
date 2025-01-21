3d1091c1cd130e6882f797855946b12ad46176f1
Gabriel Cordeiro <120519526+GabrielFRCordeiro@users.noreply.github.com>
2025-01-21 15:18:25 -0300

    feat(header): fazendo componentes header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,11 +1,20 @@
+import { Link } from "react-router-dom"
+import '../styles/components/header.css'
+
 function Header() {
   return (
     <header>
         <nav class='navbar navegacao fs-1 w-100'>
             <div class="d-flex flex-lg-column justify-content-around justify-content-lg-start w-100 align-items-lg-center">
-                <a href="./home.html" class="mt-lg-5"><i class="bi bi-house-door navIcon"></i></a>
-                <a href="./seguindo.html" class="mt-lg-4"><i class="bi bi-hand-thumbs-up navIcon"></i></a>
-                <a href="#" class="mt-lg-4"><i class="bi bi-person-circle navIcon navIconFocus"></i></a>
+                <Link to={'/home'} className="mt-lg-5" >
+                    <i className="bi bi-house-door navIcon"></i>
+                </Link>
+                <Link to={'/seguindo'} className="mt-lg-4" >
+                    <i class="bi bi-hand-thumbs-up navIcon"></i>
+                </Link>
+                <Link to={'/perfil'} className="mt-lg-4" >
+                    <i class="bi bi-person-circle navIcon navIconFocus"></i>
+                </Link>
             </div>
         </nav>
     </header>
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Entrar from "./routes/Entrar.jsx";
+import Login from "./routes/Login.jsx";
+import Cadastrar from "./routes/Cadastrar.jsx";
 import Home from "./routes/Home.jsx";
+import Seguindo from "./routes/Seguindo.jsx";
+import Perfil from "./routes/Perfil.jsx";
+import MinhaConta from "./routes/MinhaConta.jsx";
+import Desenvolvimento from "./routes/Desenvolvimento.jsx";
 
 const router = createBrowserRouter([
   {
@@ -12,8 +19,36 @@ const router = createBrowserRouter([
     children: [
       {
         path: "/",
+        element: <Entrar />,
+      },
+      {
+        path: "/login",
+        element: <Login />,
+      },
+      {
+        path: "/cadastrar",
+        element: <Cadastrar />,
+      },
+      {
+        path: "/home",
         element: <Home />,
-      }
+      },
+      {
+        path: "/seguindo",
+        element: <Seguindo />,
+      },
+      {
+        path: "/perfil",
+        element: <Perfil />,
+      },
+      {
+        path: "/minha-conta",
+        element: <MinhaConta />
+      },
+      {
+        path: "/desenvolvimento",
+        element: <Desenvolvimento />,
+      },
     ],
   },
 ]);
