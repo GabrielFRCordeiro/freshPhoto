2c8b7b6c4e4e11c77c0bcbc5f2ec4a68201c5951
Gabriel Cordeiro <120519526+GabrielFRCordeiro@users.noreply.github.com>
2025-01-30 19:34:23 -0300

    feat(cards): estilizando cards

--- frontend/js/exibir_cards_home.js
+++ frontend/js/exibir_cards_home.js
@@ -13,10 +13,22 @@ window.addEventListener('load', async (e) => {
 function show_cards(cards) {
     feed_cards.innerHTML = ''
     cards.forEach(card => {
-        feed_cards.innerHTML += `<div class="feed_card d-flex justify-content-center" role="button">
-                <div class="d-flex align-items-center post_receita">
-                    <img src="data:image/png;base64,${card.postagem_base64}" alt="" class="lazy">
+        feed_cards.innerHTML += `<div class="feed_card d-flex flex-column align-items-center">
+                <div>
+                    <a href="./outro-perfil.html" class="post_user d-flex align-items-center">
+                        <img src="data:image/png;base64,${card.usuario_base64}" alt="perfil do usuario">
+                        <p class="ps-3">@${card.usuario}</p>
+                    </a>
+                    <div class="d-flex align-items-center align-self-center my-2 post_receita">
+                        <img src="data:image/png;base64,${card.postagem_base64}" alt="postagem do usuario" class="lazy">
+                    </div>
+                    <div class="post_info d-flex justify-content-between align-items-center mb-5">
+                        <p class="p-2">${card.categoria}</p>
+                        <button>
+                            <img src="../assets/icon_receita.svg" alt="botao para ver receita">
+                        </button>
+                    </div>
                 </div>
-            </div>`
-    });
+            </div>
+    `});
 }
--- frontend/js/exibir_cards_perfil.js
+++ frontend/js/exibir_cards_perfil.js
@@ -13,10 +13,18 @@ window.addEventListener('load', async (e) => {
 function show_cards(cards) {
     feed_cards.innerHTML = ''
     cards.forEach(card => {
-        feed_cards.innerHTML += `<div class="feed_card d-flex justify-content-center" role="button">
-                <div class="d-flex align-items-center post_receita">
-                    <img src="data:image/png;base64,${card.postagem_base64}" alt="" class="lazy">
+        feed_cards.innerHTML += `<div class="feed_card d-flex flex-column align-items-center">
+                <div>
+                    <div class="d-flex align-items-center align-self-center my-2 post_receita">
+                        <img src="data:image/png;base64,${card.postagem_base64}" alt="postagem do usuario" class="lazy">
+                    </div>
+                    <div class="post_info d-flex justify-content-between align-items-center mb-5">
+                        <p class="p-2">${card.categoria}</p>
+                        <button>
+                            <img src="../assets/icon_receita.svg" alt="botao para ver receita">
+                        </button>
+                    </div>
                 </div>
-            </div>`
-    });
+            </div>
+    `});
 }
