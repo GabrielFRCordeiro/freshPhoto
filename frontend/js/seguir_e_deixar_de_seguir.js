5b381a804dcb7b0176a65e245aca62b67a39692e
Gabriel Cordeiro <120519526+GabrielFRCordeiro@users.noreply.github.com>
2025-01-31 17:29:02 -0300

    feat(seguir): criando vinculo de post

---
+++ frontend/js/seguir_e_deixar_de_seguir.js
@@ -0,0 +1,18 @@
+const btn_seguir = document.querySelector("#btn_seguir");
+
+const API_URL_VALIDAR_SEGUIR = `http://127.0.0.1:5000/seguindo/${JSON.parse(sessionStorage.getItem('usuario'))}`;
+const API_URL_SEGUIR = 'http://127.0.0.1:5000/seguindo';
+
+btn_seguir.addEventListener('click', async e => {
+    const response = await fetch(API_URL_VALIDAR_SEGUIR);
+    const seguido = await response.json();
+    const vinculo_seguir = {
+        seguido_id: JSON.parse(sessionStorage.getItem('usuario-clicado')),
+        seguidos_id: JSON.parse(sessionStorage.getItem('usuario'))
+    }
+    await fetch(API_URL_SEGUIR, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(vinculo_seguir)
+    })
+})
