38b6e4308de956d61fde20a2e7c7469f99630f97
Gabriel Cordeiro <120519526+GabrielFRCordeiro@users.noreply.github.com>
2025-02-01 12:54:59 -0300

    feat(navegacao): alterando navegacao logado

--- frontend/js/validar_sessao.js
+++ frontend/js/validar_sessao.js
@@ -1,12 +1,14 @@
 const btn_menu = document.querySelector("#btn_menu_desktop");
 const sem_sessao = document.querySelector("#modal_entrar");
 const sem_sessao_nav = document.querySelector("#modal_entrar_nav");
+const modal_config_nav = document.querySelector("#modal_config_nav");
 const btn_login = document.querySelector("#btn_login");
 const btn_cadastrar = document.querySelector("#btn_cadastrar");
 const sessao_validada = sessionStorage.getItem('usuario');
 
 if (sessao_validada) {
     btn_menu_desktop.classList.add('d-lg-inline-block');
+    modal_config_nav.classList.add('d-lg-block');
 } else {
     // sem_sessao.classList.add('d-lg-inline-block');
     sem_sessao_nav.classList.add('d-lg-block');
