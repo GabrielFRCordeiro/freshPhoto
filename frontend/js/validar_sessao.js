dbc554c426e507a048c1e53308624fe9dd5efe11
Gabriel Cordeiro <120519526+GabrielFRCordeiro@users.noreply.github.com>
2025-02-01 12:45:25 -0300

    feat(navegacao): alterando navegacao sem sessao

--- frontend/js/validar_sessao.js
+++ frontend/js/validar_sessao.js
@@ -1,5 +1,6 @@
 const btn_menu = document.querySelector("#btn_menu_desktop");
 const sem_sessao = document.querySelector("#modal_entrar");
+const sem_sessao_nav = document.querySelector("#modal_entrar_nav");
 const btn_login = document.querySelector("#btn_login");
 const btn_cadastrar = document.querySelector("#btn_cadastrar");
 const sessao_validada = sessionStorage.getItem('usuario');
@@ -7,7 +8,8 @@ const sessao_validada = sessionStorage.getItem('usuario');
 if (sessao_validada) {
     btn_menu_desktop.classList.add('d-lg-inline-block');
 } else {
-    sem_sessao.classList.add('d-lg-inline-block');
+    // sem_sessao.classList.add('d-lg-inline-block');
+    sem_sessao_nav.classList.add('d-lg-block');
     modal_entrar.style.position = 'absolute'
     modal_entrar.style.width = '16rem'
     modal_entrar.style.height = '100%'
