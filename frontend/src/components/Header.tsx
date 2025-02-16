628ceeaf78eb253405aa2dc40f229f9ea4e3ab20
Gabriel Cordeiro <120519526+GabrielFRCordeiro@users.noreply.github.com>
2025-02-16 19:41:07 -0300

    feat(Header): estilizando navegacao

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -5,11 +5,11 @@ import Logo from '../assets/img_logo.svg'
 export default function Header() {
   return (
     <header className='navegacao d-flex flex-lg-column justify-content-around justify-content-lg-start align-items-lg-center align-items-xl-start px-lg-3 px-xxl-4'>
-        <Link to={'/home'} className='nav_logo d-none d-lg-flex align-items-center justify-content-center justify-content-xl-start mt-3 w-100'>
-            <img src={Logo} alt="Logo do Fresh Photo" className='d-none d-lg-inline' />
-            <h2 className="d-none d-xl-inline mb-0">Fresh Photo</h2>
+        <Link to={'/home'} className='nav_logo d-none d-lg-flex align-items-center justify-content-center justify-content-lg-start mt-3 w-100'>
+            <img src={Logo} alt="Logo do Fresh Photo" className='d-none d-lg-inline me-2' />
+            <h2 className="d-none d-lg-inline mb-0">Fresh Photo</h2>
         </Link>
-        <nav className='d-flex flex-lg-column justify-content-around justify-content-lg-start w-100 align-items-lg-center align-items-xl-start'>
+        <nav className='d-flex flex-lg-column justify-content-around justify-content-lg-start w-100 align-items-lg-start'>
             <Link to={'/home'} className='mt-lg-5 d-flex align-items-center'>
                 <i className="bi bi-house-door"></i>
                 <p className="mb-0 ms-2">Home</p>
@@ -26,7 +26,7 @@ export default function Header() {
                 <i className="bi bi-plus-circle"></i>
                 <p className="mb-0 ms-2">Criar</p>
             </button>
-            <Link to={'/perfil'} className='mt-lg-5 d-flex align-items-center'>
+            <Link to={'/perfil'} className='my-lg-5 d-flex align-items-center'>
                 <i className="bi bi-person-circle"></i>
                 <p className="mb-0 ms-2">Perfil</p>
             </Link>
